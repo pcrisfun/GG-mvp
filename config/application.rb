217e5fc6d397559e5f5acb0648b4cf1a4f5ac971
cheyenneweaver <cheyenneweaver@gmail.com>
2012-12-14 16:22:53 -0600

    Smartbiz theme initial commit

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Added for fonts awesome to work (http://stackoverflow.com/questions/11052398/rails-using-font-awesome)
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
