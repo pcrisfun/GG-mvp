4b9e97440abd2080fc01ea2d2811fa7f3737b092
ddgriffin <dianadgriffin@gmail.com>
2013-02-25 23:21:57 -0600

    maybe added Pony too, but need to check when actually online

--- app/controllers/work_signups_controller.rb
+++ app/controllers/work_signups_controller.rb
@@ -17,7 +17,7 @@ class WorkSignupsController < ApplicationController
 
     if @work_signup.process_workshop_fee
       if @work_signup.save
-        if @work_signup.submit
+        if @work_signup.submit && @work_signup.deliver
           redirect_to workshops_path, :flash => { :success => "Awesome, you're all signed up to work with #{@workshop.host_firstname}." }
         else
           flash.now[:warning] = "Welp, that submit didn't work."
--- app/models/work_signup.rb
+++ app/models/work_signup.rb
@@ -6,4 +6,17 @@ validates :waiver, :acceptance => true
   def is_parent
     :parent == true
   end
+
+  def deliver
+    return false unless valid?
+    Pony.mail({
+      :to => "#{user.name}<#{user.email}>",
+          :from => "GirlsGuild<hello@girlsguild.com>",
+      :reply_to => "GirlsGuild<hello@girlsguild.com>",
+      :subject => "You signed up for #{event.topic} with #{event.host_firstname} #{event.host_lastname}",
+      :html_body => %(<h1>Thanks!</h1> <p>You're all signed up for '<a href="#{url_for(self.event)}">#{event.title}</a>. (Fill out this email with more info, etc)</p>,
+      :bcc => "hello@girlsguild.com",
+    })
+    return true
+  end
 end
