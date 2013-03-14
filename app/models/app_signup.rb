9e380cc3c3a4c1ef561c2d1d77c20f2751f8164f
ddgriffin <dianadgriffin@gmail.com>
2013-03-13 23:26:40 -0500

    got age validations on show view sorted out, set up Pony for decline, accept, confirm

--- app/models/app_signup.rb
+++ app/models/app_signup.rb
@@ -26,7 +26,6 @@ class AppSignup < Signup
   end
 
   def deliver_save
-    return false unless valid?
     Pony.mail({
       :to => "#{user.name}<#{user.email}>",
           :from => "GirlsGuild<hello@girlsguild.com>",
@@ -45,10 +44,46 @@ class AppSignup < Signup
           :from => "GirlsGuild<hello@girlsguild.com>",
       :reply_to => "GirlsGuild<hello@girlsguild.com>",
       :subject => "Application for #{event.title}",
-      :html_body => %(<h1>Thanks!</h1> <p>You've applied for '<a href="#{url_for(self.event)}">#{event.title}</a>. (Fill out this email with more info, etc)</p>),
+      :html_body => %(<h1>Thanks!</h1> <p>You've applied for <a href=#{url_for(event)}>#{event.title}</a>. You can see your application <a href=#{url_for(self)}>here</a>. #{event.host_firstname} will review it, and we'll let you know her decision within two weeks.</p>),
       :bcc => "hello@girlsguild.com",
     })
     return true
   end
-end
 
+  def deliver_decline
+    Pony.mail({
+      :to => "#{user.name}<#{user.email}>",
+          :from => "GirlsGuild<hello@girlsguild.com>",
+      :reply_to => "GirlsGuild<hello@girlsguild.com>",
+      :subject => "Your application has been saved - #{self.event.title}",
+      :html_body => %(<h1>Word!</h1> <p>We're thrilled you're applying to work with #{self.event.host_firstname}! If you get stuck take a look at our <a href="http://www.girlsguild.com/faq">FAQ</a>, or feel free to respond to this email with any questions you might have!</p> <p>You can edit your application here - <a href="#{url_for(self)}"> Application for #{self.event.title}</a></p>),
+      :bcc => "hello@girlsguild.com",
+    })
+    return true
+  end
+
+  def deliver_accept
+    Pony.mail({
+      :to => "#{user.name}<#{user.email}>",
+          :from => "GirlsGuild<hello@girlsguild.com>",
+      :reply_to => "GirlsGuild<hello@girlsguild.com>",
+      :subject => "Your application has been saved - #{self.event.title}",
+      :html_body => %(<h1>Word!</h1> <p>We're thrilled you're applying to work with #{self.event.host_firstname}! If you get stuck take a look at our <a href="http://www.girlsguild.com/faq">FAQ</a>, or feel free to respond to this email with any questions you might have!</p> <p>You can edit your application here - <a href="#{url_for(self)}"> Application for #{self.event.title}</a></p>),
+      :bcc => "hello@girlsguild.com",
+    })
+    return true
+  end
+
+  def deliver_confirm
+    Pony.mail({
+      :to => "#{user.name}<#{user.email}>, #{event.user.name}<#{event.user.email}",
+          :from => "GirlsGuild<hello@girlsguild.com>",
+      :reply_to => "GirlsGuild<hello@girlsguild.com>",
+      :subject => "Your application has been saved - #{self.event.title}",
+      :html_body => %(<h1>Word!</h1> <p>We're thrilled you're applying to work with #{self.event.host_firstname}! If you get stuck take a look at our <a href="http://www.girlsguild.com/faq">FAQ</a>, or feel free to respond to this email with any questions you might have!</p> <p>You can edit your application here - <a href="#{url_for(self)}"> Application for #{self.event.title}</a></p>),
+      :bcc => "hello@girlsguild.com",
+    })
+    return true
+  end
+
+end
