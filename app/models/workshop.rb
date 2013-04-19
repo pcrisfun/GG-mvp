980b9d5112c71f5eaae9478eb598285f1e50852e
ddgriffin <dianadgriffin@gmail.com>
2013-04-19 15:33:03 -0500

    scheduled reminder/followup emails built as rake tasks

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -107,7 +107,31 @@ class Workshop < Event
 			:bcc => "hello@girlsguild.com",
 		})
 		return true
-	end
+  end
+
+  def deliver_maker_reminder
+    Pony.mail({
+      :to => "#{user.name}<#{user.email}>",
+      :from => "Diana & Cheyenne<hello@girlsguild.com>",
+      :reply_to => "GirlsGuild<hello@girlsguild.com>",
+      :subject => "Your workshop is coming up! - #{self.title}",
+      :html_body => %(<h1>3, 2, 1... it's almost time!</h1> <p>Just a reminder that your workshop is happening on #{self.begins_at}. (Fill out this email with more info)</p>),
+      :bcc => "hello@girlsguild.com",
+    })
+    return true
+  end
+
+  def deliver_maker_followup
+    Pony.mail({
+      :to => "#{user.name}<#{user.email}>",
+      :from => "Diana & Cheyenne<hello@girlsguild.com>",
+      :reply_to => "GirlsGuild<hello@girlsguild.com>",
+      :subject => "How was your workshop? - #{self.title}",
+      :html_body => %(<h1>Hey #{user.first_name}!</h1> <p>How did your workshop go? We'd love to hear your feedback on the teaching experience. (Fill out this email with more info)</p>),
+      :bcc => "hello@girlsguild.com",
+    })
+    return true
+  end
 
 	def self.complete_workshop
     Workshop.where('begins_at <= ?', Date.today).all.each do |workshop|
@@ -122,6 +146,22 @@ class Workshop < Event
 			w.cancel! unless w.min_capacity_met?
 		end
 	end
+
+  def self.maker_reminder
+    Workshop.all.each do |work|
+      if (work.begins_at) == (Date.today + 3.days)
+        work.deliver_maker_reminder
+      end
+    end
+  end
+
+  def self.maker_followup
+    Workshop.all.each do |work|
+      if (work.begins_at) == (Date.today - 3.days)
+        work.deliver_maker_followup
+      end
+    end
+  end
 
 	state_machine :state, :initial => :started do
 		event :complete do
