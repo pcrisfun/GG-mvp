5837a0494efbb8e08395f99a143d7f919c31d3f6
Scott Gerlach <swgerlach@gmail.com>
2013-05-17 23:09:41 -0500

    yet another charge amount

--- app/models/event.rb
+++ app/models/event.rb
@@ -60,7 +60,7 @@ class Event < ActiveRecord::Base
     logger.info "Processing payment"
     unless charge_id.present?
       charge = Stripe::Charge.create(
-        :amount => 050,#900, # amount in cents, again
+        :amount => 100,#900, # amount in cents, again
         :currency => "usd",
         :card => stripe_card_token,
         :description => "Apprenticeship payment from #{self.user.email}"
