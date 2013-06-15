08ea34fe6001a7ad855155ae92c89fedd354daaf
cheyenneweaver <cheyenneweaver@gmail.com>
2013-06-14 19:16:58 -0500

    Post user testing edits, separate FAQs, add favicons

--- config/routes.rb
+++ config/routes.rb
@@ -71,7 +71,8 @@ GirlsGuild::Application.routes.draw do
   match '/makers', to: 'makers#show'
   match '/stripe/webhook', to: 'stripe#webhook'
 
-  match '/faq', to: 'static_pages#faq'
+  match '/faq_girls', to: 'static_pages#faq_girls'
+  match '/faq_makers', to: 'static_pages#faq_makers'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/thankyou', to: 'static_pages#thankyou'
