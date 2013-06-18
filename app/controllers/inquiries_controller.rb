2b5c470976e45de7b0e170f22918ff2c8ede4f03
cheyenneweaver <cheyenneweaver@gmail.com>
2013-06-18 13:40:30 -0500

    Flash contact page signup, re-des confirmation page, add tips to FAQ, remove Portfolio link

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -5,7 +5,7 @@ class InquiriesController < ApplicationController
 	def create
 		@inquiry = Inquiry.new(params[:inquiry])
 		if @inquiry.deliver
-			render 'static_pages/thanks'
+			redirect_to contact_path, flash: { success: "Word. Thanks for sending us an email! We'll get back to you as soon as we possibly can." }
 		else
 			render 'static_pages/contact'
 		end
