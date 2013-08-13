6ae4b9ac452b845fe6b0658354a676c477ceffdc
cheyenneweaver <cheyenneweaver@gmail.com>
2013-08-13 11:59:30 -0500

    Change http to https to avoid sign-in redirect loops, and update Google Analytics links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
+    sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'https')
     if request.referer == sign_in_url
       session[:previous_url] || root_path
     else
