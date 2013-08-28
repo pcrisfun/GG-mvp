6d2aa358420665fd79d8579b31ebf0beb6d7efbf
ddgriffin <dianadgriffin@gmail.com>
2013-08-28 15:59:18 -0500

    turned maker emails back on for workshop signups

--- app/controllers/work_signups_controller.rb
+++ app/controllers/work_signups_controller.rb
@@ -88,11 +88,11 @@ class WorkSignupsController < ApplicationController
       @work_signup.process_signup!
       @charge = Stripe::Charge.retrieve(@work_signup.charge_id)
       if @work_signup.parent?
-        @work_signup.signup && @work_signup.deliver_parent(payment: @charge) #&& @work_signup.deliver_maker_daughter
+        @work_signup.signup && @work_signup.deliver_parent(payment: @charge) && @work_signup.deliver_maker_daughter
       elsif @work_signup.minor?
-        @work_signup.signup && @work_signup.deliver_minor(payment: @charge) #&& @work_signup.deliver_maker
+        @work_signup.signup && @work_signup.deliver_minor(payment: @charge) && @work_signup.deliver_maker
       else
-        @work_signup.signup && @work_signup.deliver(payment: @charge) #&& @work_signup.deliver_maker
+        @work_signup.signup && @work_signup.deliver(payment: @charge) && @work_signup.deliver_maker
       end
       # redirect_to workshops_path, :flash => { :success => "Awesome, you're all signed up to work with #{@workshop.host_firstname}." }
       redirect_to payment_confirmation_work_signup_path(@work_signup), flash: { success: "Awesome, you're all signed up to work with #{@workshop.host_firstname}." } and return
