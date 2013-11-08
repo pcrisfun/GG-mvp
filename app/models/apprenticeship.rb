304cf74d4f2e46ae1e01ecc2018b01bbf1886249
ddgriffin <dianadgriffin@gmail.com>
2013-11-08 16:49:18 -0600

    Fix the cancel button on the app_signups / apprenticeships show pages so they can cancel from their application or the apprenticeship page

--- app/models/apprenticeship.rb
+++ app/models/apprenticeship.rb
@@ -255,11 +255,11 @@ class Apprenticeship < Event
   	end
   end
 
-  def application_confirmed?(user)
-    if already_applied?(user) && self.signups.where(user_id: user).where(:state => ["confirmed", "completed"])
-      return true
-    end
-  end
+  # def application_confirmed?(user)
+  #   if already_applied?(user) && self.signups.where(user_id: user).where(:state => ["confirmed", "completed"])
+  #     return true
+  #   end
+  # end
 
   def checkmarks
     checkmarks = {}
