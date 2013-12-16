53adbc9965a9a14e34db0859bd2cd7751494e641
ddgriffin <dianadgriffin@gmail.com>
2013-12-16 15:02:22 -0600

    un-commented development line from airbrake.rb, cause the airbrake dude said to

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,5 @@
 Airbrake.configure do |config|
   config.api_key = '8698e704b0a23247a73802f2320ed9d8'
   config.rescue_rake_exceptions = true
-  #config.development_environments = []
+  config.development_environments = []
 end
