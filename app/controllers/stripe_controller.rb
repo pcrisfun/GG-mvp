2bfd08fa07683a2a31052cb93ef3dcad6ef72620
ddgriffin <dianadgriffin@gmail.com>
2013-12-16 19:11:40 -0600

    commented out emails triggered from stripe_controller

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -12,13 +12,13 @@ class StripeController < ApplicationController
 
     if event.type == 'charge.succeeded'
       app_signup = AppSignup.find_by_charge_id(event.data.object.id)
-      app_signup.deliver_confirm(payment: event.data.object) if app_signup
+      #app_signup.deliver_confirm(payment: event.data.object) if app_signup
 
       work_signup = WorkSignup.find_by_charge_id(event.data.object.id)
-      work_signup.deliver(payment: event.data.object) if work_signup
+      #work_signup.deliver(payment: event.data.object) if work_signup
 
       app = Apprenticeship.find_by_charge_id(event.data.object.id)
-      app.deliver(payment: event.data.object) if app
+     # app.deliver(payment: event.data.object) if app
 
       raise "Unable to find model for charge_id: #{event.data.object.id}" unless app_signup || work_signup || app
     end
