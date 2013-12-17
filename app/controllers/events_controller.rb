14f42af66be9773b87ea936ecc4b309859933c5a
ddgriffin <dianadgriffin@gmail.com>
2013-12-17 16:48:26 -0600

    fixing logic to show filled events above the past events section on the index page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,8 @@ class EventsController < ApplicationController
   def index
     @events = Event.where( datetime_tba: false, state: ['accepted']).sort_by { |e| e.begins_at }
     @tba_events = Event.where( datetime_tba: true, state: ['accepted']).sort_by { |e| e.created_at }
-    @closed_events = Event.where( datetime_tba: false, state: ['filled','completed']).where("begins_at < :today", {today: Date.today}).sort_by { |e| e.begins_at }.reverse!
+    @filled_events = Event.where( datetime_tba: false, state: ['filled']).sort_by { |e| e.begins_at }
+    @closed_events = Event.where( datetime_tba: false, state: ['completed']).sort_by { |e| e.begins_at }.reverse!
   end
 
 end
