665380c7253c24af914910c97a4a4aeb2b3ad7d6
ddgriffin <dianadgriffin@gmail.com>
2014-01-06 16:10:11 -0600

    adding Airbrake api_key

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,5 @@
 Airbrake.configure do |config|
-  config.api_key = 'AIRBRAKE_API_KEY'
+  config.api_key = '0e334dd554ede0ac85cd7734aa2de148'
   config.rescue_rake_exceptions = true
   # config.development_environments = []
-  # reports on all environments
 end
