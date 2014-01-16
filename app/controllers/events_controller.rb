437ebc33920187eabc7198cdc169a56bf3c8be06
ddgriffin <dianadgriffin@gmail.com>
2014-01-16 17:39:06 -0600

    er, fixed it so closed apprenticeships don't show up in reverse order?

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,8 +3,8 @@ class EventsController < ApplicationController
   def index
     @events = Event.where( datetime_tba: false, state: ['accepted']).sort_by { |e| e.begins_at }
     @tba_events = Event.where( datetime_tba: true, state: ['accepted']).sort_by { |e| e.created_at }
-    @filled_events = Event.where( datetime_tba: false, state: ['filled']).sort_by { |e| e.begins_at }
-    @closed_events = Event.where( datetime_tba: false, state: ['completed']).sort_by { |e| e.begins_at }
+    @filled_events = Event.where( datetime_tba: false, state: ['filled']).sort_by { |e| e.begins_at }.reverse!
+    @closed_events = Event.where( datetime_tba: false, state: ['completed']).sort_by { |e| e.begins_at }.reverse!
   end
 
 end
