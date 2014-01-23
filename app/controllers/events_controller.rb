cec952381a9516ecd0ea493f61bab49450458f07
ddgriffin <dianadgriffin@gmail.com>
2014-01-22 19:02:36 -0600

    whoops, sorted events page by date created, but now making it in reverse

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
 
   def index
-    @events = Event.where( datetime_tba: false, state: ['accepted']).sort_by { |e| e.created_at }
+    @events = Event.where( datetime_tba: false, state: ['accepted']).sort_by { |e| e.created_at }.reverse!
     @tba_events = Event.where( datetime_tba: true, state: ['accepted']).sort_by { |e| e.created_at }
     @filled_events = Event.where( datetime_tba: false, state: ['filled']).sort_by { |e| e.begins_at }.reverse!
     @closed_events = Event.where( datetime_tba: false, state: ['completed']).sort_by { |e| e.begins_at }.reverse!
