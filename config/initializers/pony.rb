bbde7874ea9627dce7c0529a9db7ee71d4ed72e7
ddgriffin <dianadgriffin@gmail.com>
2014-04-19 17:55:44 -0500

    Fixed ruby version w/ Hackhands & updated config variables

--- config/initializers/pony.rb
+++ config/initializers/pony.rb
@@ -9,8 +9,8 @@ Pony.options = {
 		:port => '587',
 		:enable_starttls_auto => true,
 		:user_name => ENV["MANDRILL_USERNAME"],
-		:password => ENV["MANDRILL_APIKEY"],
-		:authentication => :plain, # :plain, :login, :cram_md5, no auth by default
+		:password => ENV["MANDRILL_API_KEY"],
+		:authentication => :login, # :plain, :login, :cram_md5, no auth by default
 		:domain => "girlsguild.com" # the HELO domain provided by the client to the server
 	}
 
