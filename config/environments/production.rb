a50b5f102106b422ad5cf77e98823ec686e8a0f5
ddgriffin <dianadgriffin@gmail.com>
2014-05-08 18:20:13 -0500

    removing fixes that didn't fix the Font-Awesome issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,6 @@ GirlsGuild::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.action_controller.relative_url_root  = "/"
-
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
