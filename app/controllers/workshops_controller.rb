fa0a73e5938ff1c96d3a48950bf5ac45537b1c7d
ddgriffin <dianadgriffin@gmail.com>
2014-05-09 18:16:42 -0500

    Made the order of events, apprenticeships, and workshops display consistently on the home page, apprenticeships page, and workshops page

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -13,7 +13,7 @@ class WorkshopsController < ApplicationController
       @allfilled_workshops = Workshop.find_all_by_state('filled').sort_by { |e| e.begins_at }
       @allcompleted_workshops = Workshop.find_all_by_state('completed').sort_by { |e| e.begins_at }
     end
-    @workshops = Workshop.where( datetime_tba: false, state: ['accepted']).sort_by { |e| e.begins_at }
+    @workshops = Workshop.where( datetime_tba: false, state: ['accepted']).sort_by { |e| e.created_at }.reverse!
     @tba_workshops = Workshop.where( datetime_tba: true, state: ['accepted']).sort_by { |e| e.created_at }
     @closed_workshops = Workshop.where( datetime_tba: false, state: ['filled','completed']).where("begins_at < :today", {today: Date.today}).sort_by { |e| e.begins_at }.reverse!
 
