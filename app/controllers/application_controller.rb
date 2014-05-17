2438af59deb3d74e99b4daf0ed9e9770fa07fe0a
cheyenneweaver <cheyenneweaver@gmail.com>
2014-05-16 19:18:37 -0500

    Fix Devise compatibility errors and style!!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,13 +11,15 @@ class ApplicationController < ActionController::Base
   after_filter :store_location
 
   def store_location
-   # store last url - this is needed for post-login redirect to whatever the user last visited.
-      if (request.fullpath != "/users/sign_in" && \
-          request.fullpath != "/users/sign_up" && \
-          !request.xhr?) # don't store ajax calls
-        session[:previous_url] = request.fullpath
-      end
+   if (!request.fullpath.match("/users") &&
+    !request.xhr?) # don't store ajax calls
+    session[:previous_url] = request.fullpath
+   end
   end
+
+def after_sign_in_path_for(resource)
+  session[:previous_url] || root_path
+end
 
   def after_sign_in_path_for(resource)
     if Rails.env.production?
