0e2d795f7a3f59b859b9efaf2afd575fb41ca108
ddgriffin <dianadgriffin@gmail.com>
2014-05-22 14:44:17 -0500

    Update "MANDRILL_APIKEY" to "MANDRILL_API_KEY" for consistency across environment/heroku config vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ GirlsGuild::Application.configure do
     :address   => "smtp.mandrillapp.com",
     :port      => 587,
     :user_name => ENV["MANDRILL_USERNAME"],
-    :password  => ENV["MANDRILL_APIKEY"]
+    :password  => ENV["MANDRILL_API_KEY"]
   }
 
   # Enable threaded mode
