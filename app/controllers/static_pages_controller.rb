2b234d7967ccb9b7cac8dc5332bd5a965e39bc6a
ddgriffin <dianadgriffin@gmail.com>
2014-10-18 11:46:45 -0500

    added ridiculous fix for home page featured events display

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,8 @@ class StaticPagesController < ApplicationController
   def home
     @featured_events = Event.where(featured: true).sort_by { |e| e.begins_at }.reverse!.take(4)
     if @featured_events.count < 4
-      @events = Event.where( datetime_tba: false, featured: false, state: ['accepted']).sort_by { |e| e.begins_at }.reverse!.take(4-@featured_events.count)
+      @events1 = Event.where(datetime_tba: false, featured: false, state: ['accepted']).sort_by { |e| e.begins_at }.reverse!.take(4-@featured_events.count)
+      @events2 = Event.where(datetime_tba: false, featured: nil, state: ['accepted']).sort_by { |e| e.begins_at }.reverse!.take(4-(@featured_events.count + @events1.count))
       # @tba_events = Event.where( datetime_tba: true, state: ['accepted','filled','completed']).limit(4 - @events.count).sort_by { |e| e.created_at }
     end
     #@events = Event.where( state: ['accepted','filled','completed']).where("begins_at >= :today", {today: Date.today}).limit(4).sort_by { |e| e.begins_at }
